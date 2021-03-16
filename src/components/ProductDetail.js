a455193af98ace798aa547defa70f016ac95da0f
Giancarlo M. Saverino <giancasaverino@gmail.com>
2021-03-17 00:17:39 +0100

    Re-imported CART in ProductDetail

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,6 +1,6 @@
 import "./ProductModal.css";
 
-function ProductDetail({product, setProduct, openCloseModal, isModalOpen}) {
+function ProductDetail({product, setProduct, openCloseModal, isModalOpen, cart, total, setCart, setTotal}) {
 
     return isModalOpen ? (<div className='ProductDetail'>
     <div className="modal-content">
@@ -10,6 +10,13 @@ function ProductDetail({product, setProduct, openCloseModal, isModalOpen}) {
         <footer>
             <h2>{product.price} €</h2>
         </footer>
+        <button onClick= {() => {
+
+            setCart([...cart, product]);
+            setTotal(total + product.price);
+            console.log(total)
+                        
+        }}>Aggiungi al carrello</button>
     </div></div>) : null;
 };
 
